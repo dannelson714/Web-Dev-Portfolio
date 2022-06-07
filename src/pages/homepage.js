15af4159302a33673b3dd01e4ac01d7b14e84734
Daniel Nelson <dan.s.nelson@gmail.com>
2022-06-07 16:35:53 -0400

    Added cubic-bezier function to homepage. Pleased with result

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,29 +1,27 @@
 import React from 'react';
-// import '../styles/About.css';
+import '../styles/Home.css';
 
 import danPic from '../../src/images/4056 (1).jpeg';
 
 export default function Home() {
+ 
   return (
-    <div>
-      <h1>About Page</h1>
-      <img id="about-pic" src={danPic} alt="Daniel in field at sundown"></img>
-      <p>
-      Developer Daniel Nelson comes to the world of full-stack coding by a circuitous path. Born and raised in Scotch Plains, New Jersey, 
-      Daniel set out to pursue a STEM education at Cornell University, but ultimately found his way into music composition. After achieving 
-      a PhD from the University of Pennsylvania in 2010 he continued to work as a composer, arranger, orchestrator and performer, always 
-      staying on the cutting edge of music software.
-      </p>
-      <br>
-      </br>
-      <p>
-      In 2021, following a deep personal loss, Daniel returned to the world of tech, diving into computer science by way of the coding 
-      language, Python. Seeking structure and rigor for his working, he commenced a course of study in 2022 with the University of
-      Pennsylvania Full-Stack Web Development certificate program. Now he works in each new project to join together the disparate strands
-      of creativity, thoughtful architecture, careful process, and audience empathy that have characterized his artistic and technical
-      achievements throughout his life.
-      </p>
-      
+    <div className="panels">
+      <div className="panel panel1">
+        <p>Let's</p>
+      </div>
+      <div className="panel panel2">
+        <p>Take</p>
+      </div>
+      <div className="panel panel3">
+        <p>It</p>
+      </div>
+      <div className="panel panel4">
+        <p>All</p>
+      </div>
+      <div className="panel panel5">
+        <p>In</p>
+      </div>
     </div>
   );
 }
