6d403b21450efc4745df5fc659a4a89de7b5f39b
Daniel Nelson <dan.s.nelson@gmail.com>
2022-06-15 10:26:14 -0400

    styled Experiences page

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -3,6 +3,7 @@ import Header from './components/header';
 import Footer from './components/footer';
 import backgroundPic from '../images/IMG_2038 3.jpeg'
 import CV from '../images/CV.pdf';
+import "../styles/Skills.css"
 
 export default function Skills() {
   return (
@@ -18,18 +19,25 @@ export default function Skills() {
         // position: "relative",
         }}>
       </div>
-      <h1>Please <a href={CV} style = {{color: "black",}}>view</a> my resume.</h1>
-      <p>
-      <ul>
-        <li>Dynamic & Reactive Web Development | React.js, JavaScript, HTML5, CSS3, Bootstrap</li>
-        <li>Data-Driven Computing | jQuery, RESTful APIs, JSON, AJAX</li>
-        <li>Back-End Development | Node.js, Express.js, MySQL, Sequelize, NoSQL (MongoDB), Insomnia</li>
-        <li>Scripting | Python, Linux bash/shell, Unit testing</li>
-        <li>Progressive Web Application | Asynchronous & Distributed Computing</li>
-        <li>General Computer Science | Data Structures, Algorithms, Resource Management</li>
-      </ul>
-      </p>
-      <div style={{height: "80px", width:"100%", clear:"both"}}></div>
+      <div className="skillsPage">
+        <br>
+        </br>
+        <h1 className="skills">Experience</h1>
+        <div className="skills-list">
+          <p>
+          <a className="resume" href={CV}>Resume</a>
+          <ul>
+            <li>Dynamic & Reactive Web Development | React.js, JavaScript, HTML5, CSS3, Bootstrap</li>
+            <li>Data-Driven Computing | jQuery, RESTful APIs, JSON, AJAX</li>
+            <li>Back-End Development | Node.js, Express.js, MySQL, Sequelize, NoSQL (MongoDB), Insomnia</li>
+            <li>Scripting | Python, Linux bash/shell, Unit testing</li>
+            <li>Progressive Web Application | Asynchronous & Distributed Computing</li>
+            <li>General Computer Science | Data Structures, Algorithms, Resource Management</li>
+          </ul>
+          </p>
+        </div>
+        <div style={{height: "80px", width:"100%", clear:"both"}}></div>
+      </div>
       <Footer />
     </div>
 
